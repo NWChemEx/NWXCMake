4dde28cd2dac91559ab69bf8f31c095c86c6056a
Murat Keceli <keceli@gmail.com>
2023-01-04 13:24:36 -0600

    Added cmake modules

---
+++ modules/FindCppyy.cmake
@@ -0,0 +1,72 @@
+#
+# This CMake module ensures that your Python interpreter has the cppyy module
+# installed. 
+#
+# Set minimal cppyy version
+#
+set(cppyy_version "2.4.1")
+#
+# Make sure Python3 is installed
+#
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+#
+# Check if cppyy Python package exists
+#
+execute_process(
+    COMMAND ${Python3_EXECUTABLE} -c "import cppyy"
+    OUTPUT_VARIABLE _fcppyy_result
+)
+#
+# If cppyy exists, check version
+#
+if("${_fcppyy_result}" STREQUAL "")
+    set(Cppyy_FOUND TRUE)
+    execute_process(
+        COMMAND ${Python3_EXECUTABLE} -c "import cppyy; print(cppyy.__version__),"
+        OUTPUT_VARIABLE _fcppyy_result2
+    )
+else()
+    set(Cppyy_FOUND FALSE)
+endif()
+#
+# Try to install cppyy Python package, if it doesn't exist or is incorrect version
+#
+if("${_fcppyy_result}" STREQUAL "" OR NOT "${_fcppyy_result2}" VERSION_EQUAL "${cppyy_version}")
+    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+        set(_pip_args)
+    else()
+        set(_pip_args "--user")
+    endif()
+    set(_pypkg_name "cppyy==${cppyy_version}")
+    execute_process(COMMAND ${Python3_EXECUTABLE} -m pip install ${_pypkg_name} ${_pip_args})
+    #
+    # Check again if cppyy Python works
+    #
+    execute_process(COMMAND ${Python3_EXECUTABLE} -c "import cppyy" OUTPUT_VARIABLE _fcppyy_result)
+    if("${_fcppyy_result}" STREQUAL "")
+        set(Cppyy_FOUND TRUE)
+    	execute_process(
+            COMMAND ${Python3_EXECUTABLE} -c "import cppyy; print(cppyy.__version__),"
+        	OUTPUT_VARIABLE _fcppyy_result2
+    	)
+    else()
+        set(Cppyy_FOUND FALSE)
+    endif()
+    #
+    # Check the version again
+    #
+    if("${_fcppyy_result2}" VERSION_EQUAL "${cppyy_version}")
+        set(Cppyy_FOUND TRUE)
+    else()
+        set(Cppyy_FOUND FALSE)
+    endif()
+else()
+    set(Cppyy_FOUND TRUE)
+endif()
+
+if(NOT Cppyy_FOUND)
+    message("Python3: ${Python3_EXECUTABLE}")
+    message("import output: ${_fcppyy_result}")
+    message("version output: ${_fcppyy_result2}")
+    message(FATAL_ERROR "CMake could not install cppyy, try installing cppyy ${cppyy_version} manually.")
+endif()
