b52214910386c0b8107b4b800de4aa0e9b49eca2
Murat Keceli <keceli@gmail.com>
2023-01-04 15:27:16 -0600

    Update headers for cmake modules

--- modules/nwx_cxx_api_docs.cmake
+++ modules/nwx_cxx_api_docs.cmake
@@ -1,13 +1,16 @@
-################################################################################
+# Copyright 2022 NWChemEx-Project
 #
-# The master version of this file lives at
-# NWChemEx-Project/DeveloperTools/cmake/nwx_cxx_api_docs.cmake. The version
-# contained in all other NWChemEx-Project repositories  is a synchronized copy
-# of the master version. Any changes made to the non-master version will be lost
-# the next time the master version is updated. To make changes please make them
-# to the master version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-################################################################################
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 include_guard()
 
