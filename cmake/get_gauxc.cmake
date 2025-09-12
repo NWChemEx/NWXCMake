c4365e99c32ffd34e8746bb2ad8a52073df12273
Ryan Richard <ryanmrichard1@gmail.com>
2025-09-12 11:40:20 -0500

    Update gauxc dependency version in CMake file

--- cmake/get_gauxc.cmake
+++ cmake/get_gauxc.cmake
@@ -1,6 +1,7 @@
 cmaize_find_or_build_dependency(
     gauxc
     URL github.com/wavefunction91/GauXC
+    VERSION "${NWX_GAUXC_VERSION}"
     BUILD_TARGET gauxc
     FIND_TARGET gauxc::gauxc
     CMAKE_ARGS BUILD_TESTING=OFF 
